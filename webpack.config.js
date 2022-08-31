55a6de9804b3a73dca958ca4107544cd6e37a961
alenart91 <alenart91@gmail.com>
2022-08-31 16:23:06 -0500

    fixed css FOUC issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: 'development',
@@ -25,7 +26,7 @@ module.exports = {
       title: 'Sorting Visualizer',
       template: 'src/template.html',
     }),
-  ],
+  new MiniCssExtractPlugin()],
   output: {
     filename: '[name][contenthash].js',
     path: path.resolve(__dirname, 'dist'),
@@ -35,7 +36,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
     ],
   },
