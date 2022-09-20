6e7ae4f08a3255176d771d2a8f413f0f27beefee
alenart91 <alenart91@gmail.com>
2022-09-20 12:41:07 -0500

    insertion sort with rAF added

---
+++ src/algorithms/insertionSort.js
@@ -0,0 +1,223 @@
+let insertionSort = (myArray, speed, continueSort, callback) => {
+
+  // create worker when function is invoked
+  const worker = new Worker(new URL('insertionWorker.js', import.meta.url));
+
+  worker.postMessage(myArray);
+
+  worker.onmessage = (e) => {
+
+    // stop the function when stop button is clicked
+    if(continueSort.continue === false) {
+      worker.terminate();
+      return callback();
+    }
+
+    if(e.data.method === 'color') {
+      addColor(e.data.status, e.data.array, e.data.j, e.data.temp, e.data.itemOne, e.data.itemTwo);
+    }
+
+    if(e.data.method === 'swap') {
+      swap(e.data.itemOne, e.data.itemTwo);
+    }
+
+    if(e.data.method === 'sorted') {
+      sorted(e.data.array, e.data.i, e.data.temp, e.data.itemTwo);
+    }
+
+    if(e.data.message === 'finished') {
+
+      // kill worker thread
+      worker.terminate();
+      return callback();
+    }
+  };
+
+
+
+  // adds color for sorted items
+  async function sorted(array, i, temp, itemTwo) {
+
+    let secondChild = document.getElementById(itemTwo);
+    let tempChild = document.getElementById(temp);
+
+
+     if(i === 1) {
+
+       // on the first iteration set both items to sorted whether or not they're swapped
+       if(secondChild === null) {
+         tempChild.setAttribute('data-sorted', 'true');
+
+         let jChild = document.getElementById(array[i]);
+         jChild.setAttribute('data-sorted', 'true');
+       } else {
+         tempChild.setAttribute('data-sorted', 'true');
+         secondChild.setAttribute('data-sorted', 'true');
+         }
+     } else {
+         tempChild.setAttribute('data-sorted', 'true');
+     }
+
+    return worker.postMessage({message: 'sortedFinished'});
+  }
+
+
+
+
+
+  // adds and removes colors for comparison
+  async function addColor(status, array, j, temp, itemOne, itemTwo) {
+
+    let firstChild = document.getElementById(itemOne);
+    let secondChild = document.getElementById(itemTwo);
+
+    let start;
+
+    async function animateColor(timestamp) {
+
+      if (start === undefined) {
+        start = timestamp;
+      }
+
+      const elapsed = timestamp - start;
+
+
+      if(status === 'initial compare') {
+
+        let thirdChild = document.getElementById(array[j - 2]);
+        if(thirdChild !== null && thirdChild.hasAttribute('data-compare')) {
+
+          thirdChild.removeAttribute('data-compare');
+          secondChild.removeAttribute('data-current');
+
+        }
+
+        firstChild.setAttribute('data-current', 'true');
+        secondChild.setAttribute('data-compare', 'true');
+      }
+
+
+
+      if(status === 'compare') {
+        let thirdChild = document.getElementById(array[j - 2]);
+
+        // remove previous two comparisons
+        if(thirdChild !== null && thirdChild.hasAttribute('data-compare')) {
+
+          thirdChild.removeAttribute('data-compare');
+          secondChild.removeAttribute('data-current');
+        }
+
+        firstChild.setAttribute('data-current', 'true');
+
+        // remove the sorted color temporarily to show it being compared
+        if(secondChild.hasAttribute('data-sorted')) {
+          secondChild.removeAttribute('data-sorted');
+        }
+
+        secondChild.setAttribute('data-compare', 'true');
+      }
+
+
+      if(status === 'after swap') {
+
+        firstChild.removeAttribute('data-compare');
+
+        // set the sorted color back after swapping
+        firstChild.setAttribute('data-sorted', 'true');
+      }
+
+
+      if(elapsed > speed) {
+
+        return worker.postMessage({message: 'colorFinished'});
+
+       } else {
+          window.requestAnimationFrame(animateColor);
+      }
+  };
+
+    window.requestAnimationFrame(animateColor);
+  };
+
+
+
+
+  function swap(itemOne, itemTwo) {
+
+    let moveX,
+    moveY;
+
+    let transX,
+    transY;
+
+    let styleFirst,
+    styleSecond;
+
+    let x,
+    y;
+
+    let xBase,
+    yBase;
+
+    let start;
+
+
+    let firstChild = document.getElementById(itemOne);
+    let secondChild = document.getElementById(itemTwo);
+
+    // if transform property was assigned before then get it; if not set it to 0
+    firstChild.style.transform ? styleFirst = +firstChild.style.transform.replace(/[^-?\d.]/g, '') : styleFirst = 0;
+    secondChild.style.transform ? styleSecond = +secondChild.style.transform.replace(/[^-?\d.]/g, '') : styleSecond = 0;
+
+    x = firstChild.getBoundingClientRect();
+    y = secondChild.getBoundingClientRect();
+
+    xBase = (Math.round(y.x * 100) / 100) - (Math.round(x.x * 100) / 100);
+    yBase = (Math.round(x.x * 100) / 100) - (Math.round(y.x * 100) / 100);
+
+
+    function swapItems(timestamp) {
+
+      if (start === undefined) {
+        start = timestamp;
+      }
+
+      const elapsed = timestamp - start;
+      const progress = elapsed / speed;
+
+      moveX = (xBase * Math.min(progress, 1)) + styleFirst;
+      moveY = (yBase * Math.min(progress, 1)) + styleSecond;
+
+      transX = moveX.toFixed(2) + 'px';
+      transY = moveY.toFixed(2) + 'px';
+
+      firstChild.style.transform = `translateX(${transX})`;
+      secondChild.style.transform = `translateX(${transY})`;
+
+
+      if(elapsed < speed) {
+         window.requestAnimationFrame(swapItems);
+      } else {
+
+         // changes actual element order in the DOM
+         secondChild.before(firstChild);
+
+         // remove translate so element is in correct position after switch
+         firstChild.style.transform = `translateX(0px)`;
+         secondChild.style.transform = `translateX(0px)`;
+
+         return worker.postMessage({message:'animationFinished'});
+     }
+
+   };
+
+   window.requestAnimationFrame(swapItems);
+
+ };
+
+
+};
+
+
+  export default insertionSort;
---
+++ src/algorithms/insertionWorker.js
@@ -0,0 +1,110 @@
+// Data passed between the main page and workers is copied, not shared
+
+let init = false;
+let sorter;
+
+onmessage = (e) => {
+
+  if(init === false) {
+    init = true;
+    let array = e.data;
+    sorter = sort(array);
+    sorter.next();
+  }
+
+
+  if(e.data.message === 'colorFinished') {
+
+    try {
+      let nextIt = sorter.next();
+      if(nextIt.done === true) {
+
+        postMessage({message: 'finished'});
+      }
+  } catch(err) {
+      console.log(err);
+    }
+  }
+
+
+
+  if(e.data.message === 'animationFinished') {
+
+    try {
+      let nextIt = sorter.next();
+      if(nextIt.done === true) {
+
+        postMessage({message: 'finished'});
+      }
+  } catch(err) {
+      console.log(err);
+    }
+  }
+
+
+  if(e.data.message === 'sortedFinished') {
+
+    try {
+      let nextIt = sorter.next();
+      if(nextIt.done === true) {
+        postMessage({message: 'finished'});
+      }
+  } catch(err) {
+      console.log(err);
+    }
+  }
+
+}
+
+
+
+
+function* sort(myArray) {
+
+  for(let i = 1; i < myArray.length; i++) {
+
+    j = i;
+    temp = myArray[i];
+
+    yield addColor('initial compare', myArray, j, temp, myArray[j], myArray[j - 1]);
+
+    // temp is the value of i; the item that needs to be inserted
+    while(j > 0 && myArray[j - 1] > temp) {
+
+      yield addColor('compare', myArray, j, temp, myArray[j], myArray[j - 1]);
+
+      // if the previous element is bigger than the current element then switch
+      // set the value of the position of the insertion element to the number that was bigger than it
+      myArray[j] = myArray[j - 1];
+
+      yield swap(temp, myArray[j - 1]);
+
+      j--;
+      yield addColor('after swap', myArray, j, temp, myArray[j], myArray[j - 1]);
+    }
+
+  // insert the value myArray[i] wherever it is the biggest value or when there is no other items to compare
+  // which makes it the smallest by default
+
+  // j decrements so temp is inserted to where it lays in the array.
+
+  myArray[j] = temp;
+
+  yield sorted(myArray, i, temp, myArray[j - 1]);
+  }
+
+};
+
+
+
+function swap(x, y) {
+  postMessage({method: 'swap', itemOne: x, itemTwo: y});
+};
+
+function addColor(status, array, j, temp, itemOne, itemTwo) {
+    postMessage({method: 'color', status: status, array: array, j: j, temp: temp, itemOne: itemOne, itemTwo: itemTwo});
+};
+
+function sorted(array, i, temp, itemTwo) {
+  postMessage({method: 'sorted', array: array, i: i, temp: temp, itemTwo: itemTwo});
+};
